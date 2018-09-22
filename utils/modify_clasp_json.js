752e485645ea827208425a53925c1c40804647d4
HeRoMo <hero@asterisk-works.jp>
2018-09-22 23:42:33 +0900

    remove configs of webpack and babel build

--- utils/modify_clasp_json.js
+++ utils/modify_clasp_json.js
@@ -1,5 +1,8 @@
+/*
+ * This script will be probably unnecessary in clasp v1.5.4 or lator
+ */
 const fs = require('fs');
 const claspJson = require('../.clasp.json');
 
-claspJson.rootDir = 'dest';
+claspJson.rootDir = 'src';
 fs.writeFileSync('.clasp.json', JSON.stringify(claspJson));
