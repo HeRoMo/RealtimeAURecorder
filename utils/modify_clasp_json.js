35ba14d9a7f735377984c386c9f8f60ba2f32e9d
HeRoMo <hero@asterisk-works.jp>
2020-07-24 17:36:54 +0900

    uopdate scripts

--- utils/modify_clasp_json.js
+++
@@ -1,8 +0,0 @@
-/*
- * This script will be probably unnecessary in clasp v1.5.4 or lator
- */
-const fs = require('fs');
-const claspJson = require('../.clasp.json');
-
-claspJson.rootDir = 'src';
-fs.writeFileSync('.clasp.json', JSON.stringify(claspJson));
